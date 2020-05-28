065e9e73d6d0ede9dd0b6a531a7e7b5cbb36f619
ying.liu <1292474120@qq.com>
2020-05-28 15:06:09 +0800

    ly_gittest

--- src/main/java/gittest/gitTest.java
+++ src/main/java/gittest/gitTest.java
@@ -1,5 +1,10 @@
 package gittest;
 
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
 public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
 }
 
